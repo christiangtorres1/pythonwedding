55852f3a9a9432617c28e6eb64378caa17934577
[christiangtorres1] <christiangtorres23@gmail.com>
2022-09-25 17:56:59 -0500

    Final changes for today

--- weddint_python_dump.py
+++ weddint_python_dump.py
@@ -3,14 +3,16 @@
 #Variables
 
 weddingname = "Torres Wedding"
-date = "11/3/2023"
+date = "November 3 2023"
 venue = "Hayes Hollow at Hidden Falls"
 venueaddress = "2222 Bridal Veil, Sping, Texas, 78070"
 bride = "Megan F. Osborne"
 groom = "Christian G. Torres"
+groomfirst = groom.split()[0]
+bridefirst = bride.split()[0]
 
 
-#Sets
+#Dictionaries
 
 groomsmen = {
     "bestman": "Amador Yeverino",
@@ -24,12 +26,21 @@ bridesmaids =  {
     "maidofhonor": "Ashley Newheart",
     "bridesmaid1": "Lori Osborne"
 }
-#simple print examples
+
+#simple print example
 
 print(f"The {weddingname} will take place on {date} with {bride} marrying {groom} we hope you can join us!")
 
 # for loops examples
 
+print(f"{groom}'s groomsmen will be:")
 for names in groomsmen.values():
     print(names)
 
+print(f"{groomfirst}'s best man is {groomsmen.get('bestman')}")
+
+print(f"{bride}'s bridesmaids will be:")
+for names in bridesmaids.values():
+    print(names)
+
+print(f"{bridefirst}'s Maid of Honor is {bridesmaids.get('maidofhonor')}")
